cd0a0135b2b2699abf73d6bef61e15750c59f73c
Ian Pierce <iandpierce@gmail.com>
2019-09-16 23:17:20 -0500

    initial commit

---
+++ src/App.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <nav>
+          <a>Events</a>
+          <a>Profile</a>
+          <a>Plex</a>
+      </nav>
+    </div>
+  );
+}
+
+export default App;
