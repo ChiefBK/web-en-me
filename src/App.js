f772699480fa79d90f29f2ed5410597c887ce44d
Ian Pierce <iandpierce@gmail.com>
2019-09-19 18:45:32 -0500

    basic site layout

--- src/App.js
+++ src/App.js
@@ -1,56 +1,56 @@
 import React from 'react';
 import './App.css';
-import { BrowserRouter as Router, Route, Link } from "react-router-dom";
+import { BrowserRouter as Router, Route, Link, Switch } from "react-router-dom";
 
-function Home() {
-    return <h2>Home</h2>;
-}
-
-function About() {
-    return <h2>About</h2>;
-}
-
-function Topic({ match }) {
-    return <h3>Requested Param: {match.params.id}</h3>;
-}
-
-function Topics(a) {
+function Events() {
     return (
-        <div>
-            <h2>Topics</h2>
-
-            <ul>
-                <li>
-                    <Link to={`${a.match.url}/components`}>Components</Link>
-                </li>
-                <li>
-                    <Link to={`${a.match.url}/props-v-state`}>Props v. State</Link>
-                </li>
-            </ul>
-
-            <Route path={`${a.match.path}/:id`} component={Topic} />
-            <Route
-                exact
-                path={a.match.path}
-                render={() => <h3>Please select a topic.</h3>}
-            />
-        </div>
+        <h2>Events</h2>
     );
 }
 
-function Header() {
+function Plex({ match }) {
+    return <h3>Plex</h3>;
+}
+
+// function Plex(a) {
+//     return (
+//         <div>
+//             <h2>Topics</h2>
+//
+//             <ul>
+//                 <li>
+//                     <Link to={`${a.match.url}/components`}>Components</Link>
+//                 </li>
+//                 <li>
+//                     <Link to={`${a.match.url}/props-v-state`}>Props v. State</Link>
+//                 </li>
+//             </ul>
+//
+//             <Route path={`${a.match.path}/:id`} component={Topic} />
+//             <Route
+//                 exact
+//                 path={a.match.path}
+//                 render={() => <h3>Please select a topic.</h3>}
+//             />
+//         </div>
+//     );
+// }
+
+function Home() {
     return (
-        <ul>
-            <li>
-                <Link to="/">Home</Link>
-            </li>
-            <li>
-                <Link to="/about">About</Link>
-            </li>
-            <li>
-                <Link to="/topics">Topics</Link>
-            </li>
-        </ul>
+        <nav>
+            <ul>
+                <li>
+                    <Link to="/events">Events</Link>
+                </li>
+                <li>
+                    <Link to="/">Home</Link>
+                </li>
+                <li>
+                    <Link to="/plex">Plex</Link>
+                </li>
+            </ul>
+        </nav>
     );
 }
 
@@ -58,11 +58,11 @@ function App() {
   return (
       <Router>
           <div>
-              <Header />
-
-              <Route exact path="/" component={Home} />
-              <Route path="/about" component={About} />
-              <Route path="/topics" component={Topics} />
+              <Switch>
+                <Route exact path="/" component={Home} />
+                <Route path="/events" component={Events} />
+                <Route path="/plex" component={Plex} />
+              </Switch>
           </div>
       </Router>
   );
