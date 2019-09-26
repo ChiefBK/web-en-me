e5a521638f7507234833909a54be57e5d3d41d32
Ian Pierce <iandpierce@gmail.com>
2019-09-25 23:09:32 -0500

    added react-redux

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import './App.css';
+import { Provider } from 'react-redux';
 import { BrowserRouter as Router, Route, Link, Switch } from "react-router-dom";
 
 class Events extends Component {
